fa8b572a4c4823c6fb206be31af66a78f2167e12
RoshanDileep <118088610+RoshanDileep@users.noreply.github.com>
2023-07-17 16:22:29 -0700

    Add password protection
    You now have to enter a password to upload files

--- browser.js
+++ browser.js
@@ -1,7 +1,23 @@
+const passwordForm = document.getElementById("passwordForm");
+const passwordInput = document.getElementById("password");
+const submitPasswordButton = document.getElementById("submitPassword");
+const fileBrowser = document.getElementById("fileBrowser");
 const fileButton = document.getElementById("fileButton");
 const fileInput = document.createElement("input");
 fileInput.type = "file";
 fileInput.multiple = true; // Enable multiple file selection
+
+submitPasswordButton.addEventListener("click", () => {
+  const enteredPassword = passwordInput.value;
+  const correctPassword = "rickshaw"; // Set your desired password here
+
+  if (enteredPassword === correctPassword) {
+    passwordForm.style.display = "none";
+    fileBrowser.style.display = "block";
+  } else {
+    alert("Invalid password");
+  }
+});
 
 fileButton.addEventListener("click", () => {
   fileInput.click();
